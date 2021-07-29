372f921f039c46a36b3b57a07fed4088fd9166ec
pavanteja <pavanteja14@gmail.com>
2021-07-30 03:55:00 +0530

    changes to env variables

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:create_app --preload
+web: gunicorn run:app --preload
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from project import create_app
+from project import app
 
 if __name__ == "__main__":
-    create_app.run()
+    app.run()
