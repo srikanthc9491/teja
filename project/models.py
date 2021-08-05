2562a8a0508c25ce9b071b4a81100d573714e08c
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-06 01:41:56 +0530

    Update models.py

--- project/models.py
+++ project/models.py
@@ -9,6 +9,24 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+db.create_all()
+db.session.commit()
+
+admin = User('admin', 'admin@example.com')
+guest = User('guest', 'guest@example.com')
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
+user = User.query.all()
+print user
+
 
 
 
