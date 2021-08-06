d6688759ccd8771e05af1d7d9bdc8548982d3a2f
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-06 22:18:57 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -81,7 +81,7 @@ def upload_file():
     d = dfc.values.tolist()
     c = dfc.columns.tolist()
     d.insert(0,c)
-    data = json.dumps({'data':d})
+    data = json.dumps({d})
     
     return render_template("predata.html", tables=[states.to_html(classes='data', header=False)], titles = ['na', 'you have to file GSTR 1 for these states'], totalTax=totalTax, data=data) 
 
