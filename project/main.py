57f4e99b38d9c9e1aa18eaa16b786d06979a9fa1
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-08 04:00:20 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect
 from flask_login import login_required, current_user
 import pandas as pd
 import os.path
