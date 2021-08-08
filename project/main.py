dafd651b22c4416013ac1e134767005702d40f0c
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-08 09:46:54 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -5,7 +5,6 @@ import os.path
 import matplotlib.pyplot as plt
 import json
 import razorpay
-from pandas import to_json
 main = Blueprint('main', __name__)
 
 
@@ -96,12 +95,12 @@ def upload_file():
     
     dfc= df1.groupby(['Ship To State']).agg({'Total Tax Amount': ['sum']})
     data= states.to_dict('index')
-    session["data"] = dfa.to_json()
+    session["gst"] = dfa.to_json()
     return render_template("predata.html", tables=[states.to_html(classes='data', header=False)], titles = ['na', 'you have to file GSTR 1 for these states'], totalTax=totalTax, data=data) 
 
 @main.route('/data', methods= ['GET', 'POST'])
 def data():
-    dfa = session.get('data')
+    dfa = session.get('gst')
     dfa= pd.read_json(dfa)
     return render_template("data.html",tables=[dfa.to_html(classes='data', header=False)], titles = ['na', 'you have to file GSTR 1 for these states'])
 
