8058a1d3c3798fd5f24a1d679f78c2e86e579d90
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-13 16:19:02 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -106,10 +106,10 @@ def upload_file():
     data= states
     return render_template("predata.html", tables=[statestable.to_html(classes='data', header=False)], titles = ['na', 'you have to file GSTR 1 for these states'], gstin=gstin, no_orders=no_orders, totalTax=totalTax, data=data, no_refunds=no_refunds, no_cancel=no_cancel, totalSale=totalSale) 
 
-@main.route('/data', methods= ['GET', 'POST'])
-def datae():
-     if request.method == "POST":
-     dfd= pd.read_csv(session.get('gst'))
+@main.route('/data'<payment_id>, methods= ['GET', 'POST'])
+def datae(payment_id):
+     if payment_id != "":
+        dfd= pd.read_csv(session.get('gst'))
      gstin= dfd['Seller Gstin'].iloc[0]
      dfw= dfd[['Transaction Type', 'Ship To State','Order Id']]
      df= dfd[['Transaction Type', 'Ship To State', 'Tax Exclusive Gross', 'Total Tax Amount']]
