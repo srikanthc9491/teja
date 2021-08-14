fc80b637caffd98a53d1aaceda1610e0adc4138b
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-14 19:08:48 +0530

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,4 +1,4 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+from flask_wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
  
 class ContactForm(Form):
   name = TextField("Name",  [validators.Required("Please enter your name.")])
