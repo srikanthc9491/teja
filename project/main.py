943f4b9b4753d6de5ea739d9c922528fbc7c8abb
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-08-14 19:49:33 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -12,7 +12,7 @@ main = Blueprint('main', __name__)
 
 razorpay_client = razorpay.Client(auth=("rzp_test_eTLJcDvEJdeU2G", "a2TS4HG8wpO84TuiPZHiG0CR"))
 from project.models import User
-from run import db, app
+from run import db, create_app
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
