8a25320a19da63a9dd307e393c7a310b6eecc75d
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-09-28 17:49:26 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -99,8 +99,10 @@ def upload_file():
     b2bfile = request.files['ab']
     if len(b2bfile.filename) >= 2 :
         session["b2b"]=b2bfile.filename
-    b2bfile.save(b2bfile.filename)
-    Bdata = pd.read_csv(b2bfile.filename)
+        b2bfile.save(b2bfile.filename)
+        Bdata = pd.read_csv(b2bfile.filename)
+     else :
+        pass
 
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
